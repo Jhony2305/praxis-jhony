97eed81730b3e47d5d0216408b471eefebf3e14d
Jhony2305 <87538603+Jhony2305@users.noreply.github.com>
2025-02-16 12:08:18 -0500

    cambio de pagina index home

--- echooling-react-main/src/pages/home/AboutSection.js
+++ echooling-react-main/src/pages/home/AboutSection.js
@@ -18,15 +18,16 @@ const About = () => {
                     </div>
                     <div className="col-lg-6">
                         <div className="about__content">
-                            <h2 className="about__title wow animate__fadeInUp" data-wow-duration="0.3s">Praxis Capacitaciones<br/> <em>la plataforma de Praxis</em></h2>
-                            <p className="about__paragraph wow animate__fadeInUp" data-wow-duration="0.5s">Education is both the act of teaching knowledge to others and<br/> the act of receiving knowledge from someone else.</p>
-                            <p className="about__paragraph2 wow animate__fadeInUp" data-wow-duration="0.7s"> Have questions?  <Link to="#"> Get Free Guide </Link></p>
-                            <p className="wow animate__fadeInUp" data-wow-duration="0.6s">Education also refers to the knowledge received through schooling instruction <br/>and to the institution of teaching as a whole. The main purpose of education <br/>is the integral development of a person.</p>
+                            <h2 className="about__title wow animate__fadeInUp" data-wow-duration="0.3s">Somos futuro,<br/> <em>Somos Praxis</em></h2>
+                            <p className="about__paragraph wow animate__fadeInUp" data-wow-duration="0.5s">Mejora tus habilidades laborales con nuestra capacitación continua. Aprende en un ambiente amigable y alcanza el éxito profesional.<br/></p>
+                            <p className="about__paragraph2 wow animate__fadeInUp" data-wow-duration="0.7s"> Inscribete hoy?</p>
+                            <h2 className="about__title wow animate__fadeInUp" data-wow-duration="0.3s">Educación de<br/> <em>Calidad</em></h2>
+                            <p className="about__paragraph wow animate__fadeInUp" data-wow-duration="0.5s">Contamos con un equipo de expertos altamente calificados que se dedican a brindar a nuestros estudiantes una experiencia de aprendizaje enriquecedora y práctica. <br/>Ofrecemos cursos en línea y presenciales para <br/>garantizar que nuestros estudiantes tengan <br/>acceso a la capacitación que necesitan para <br/>crecer y destacarse en la industria.<br/><br/></p>
                             <ul className="wow animate__fadeInUp" data-wow-duration="0.9s">
-                                <li><Link to="/about" className="more-about"> Read More <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></Link></li>
+                                <li><Link to="/about" className="more-about"> Leer Más <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></Link></li>
                                 <li className="last-li">
-                                    <em>Get Support</em>
-                                    <Link to="mailto:support@react.com">support@react.com</Link>
+                                    <em>Soporte a</em>
+                                    <Link to="/contact">infor@gmail.com</Link>
                                 </li>
                             </ul>
                         </div>
